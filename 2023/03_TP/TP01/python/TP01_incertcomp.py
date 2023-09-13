bfee2c58ec26fc806c5e9a6b0e037b539657eb26
Nora-n <nora.nicolas@ens-lyon.org>
2023-09-14 01:28:56 +0200

    TDO3, fiches MC, intro nb et python, TP01 notebook

--- 2023/03_TP/TP01/python/TP01_incertcomp.py
+++ 2023/03_TP/TP01/python/TP01_incertcomp.py
@@ -2,9 +2,11 @@ import numpy as np
 
 d = 12               # cm
 Delta_d = 0.1        # cm
+D_d = [11.9, 12.1]   # cm
 ud = 0.1/np.sqrt(3)  # cm
 D = 50               # cm
 Delta_D = 0.5        # cm
+D_D = [49.5, 50.5]   # cm
 uD = 0.5/np.sqrt(3)  # cm
 f = D/4 - d**2/(4*D) # cm
 
@@ -16,13 +18,14 @@ uf_comp = 0.25*np.sqrt(
 
 print(f'f = {f:.2f} ± {uf_comp:.2f}')
 
-N = 10000
-flist = []
+N = 100000
+liste_f = []
 for i in range(0, N):
-    d_simu = d + Delta_d*np.random.uniform(-1, 1)
-    D_simu = D + Delta_D*np.random.uniform(-1, 1)
+    d_simu = np.random.uniform(D_d[0], D_d[1])
+    D_simu = np.random.uniform(D_D[0], D_D[1])
     f_simu = D_simu/4 - d_simu**2/(4*D_simu)
-    flist.append(f_simu)
+    liste_f.append(f_simu)
 
-uf_mc = np.std(flist, ddof=1)
-print(f'f = {f:.2f} ± {uf_mc:.2f}')
+fmoy = np.mean(liste_f)
+uf = np.std(liste_f, ddof=1)
+print(f'f = {fmoy:.2f} ± {uf:.2f}')

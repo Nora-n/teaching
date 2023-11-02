98de17a17c2dc52029e21adf1e723335574ce083
Nora-n <nora.nicolas@ens-lyon.org>
2023-11-02 16:37:03 +0100

    I06, DM01 fig. Deleted test by mégarde.

--- test/test_calc.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-grad = 1/100
-print(grad/np.sqrt(3))
--- test/test_std.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-vals = np.array([11.9, 12.5, 13.1, 12.4, 12.9, 12.8, 12.6])
-mean = np.mean(vals)
-incert = np.std(vals, ddof=1)
-print(fr'D = {mean:.2f} $\pm$ {incert:.2f}')

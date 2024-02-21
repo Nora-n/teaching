e9614500b29daabc5dc5e7ccc90c6be072d5cd69
Nora-n <nora.nicolas@ens-lyon.org>
2024-02-21 03:30:49 +0100

    TDM6-7 done. M8 todo… DS06 tocorr… TP 17-18 tocorr… 20h to go…

--- test/test.py
+++ test/test.py
@@ -1,7 +1,36 @@
 import numpy as np
 
-m = 10.4e-3    # kg
-tau = 73.5e-3  # s
-R = 1.0e-2     # m
-eta = m/(6*np.pi*tau*R)
-print(f'\\eta = {eta}')
+# TP 18
+# m = 10.4e-3    # kg
+# tau = 73.5e-3  # s
+# R = 1.0e-2     # m
+# eta = m/(6*np.pi*tau*R)
+# print(f'\\eta = {eta}')
+
+# TD M7
+G = 6.67e-11 # SI
+M = 5.97e24  # kg
+r2 = 42200e3 # m
+r1 = 7500e3  # m
+v2 = np.sqrt(G*M/r2)
+ve2 = np.sqrt(2*G*M*r1/(r2*(r1+r2)))
+Dva = v2-ve2
+print(f'v_2 = {v2:.1f}, v_e2 = {ve2:.1f}, D v_A = {Dva:.1f}')
+
+v1 = np.sqrt(G*M/r1)
+ve1 = np.sqrt(2*G*M*r2/(r1*(r1+r2)))
+Dvp = ve1-v1
+print(f'v_1 = {v1:.1f}, v_e1 = {ve1:.1f}, D v_P = {Dvp:.1f}')
+# Wpp = 17e9 # J
+# m = 2*Wpp/(ve1**2-v1**2)
+m = 1000  # kg
+print(f'm = {m:.2e} kg')
+Wp = G*m*M*(r2-r1)/(2*r1*(r1+r2))
+print(f'Wp = {Wp/1e9:.0f} GJ')
+Wa = (G*M*m*(r2-r1))/(2*r2*(r1+r2))
+print(f'Wa = {Wa/1e9:.1f} GJ')
+
+# M7
+Tsid = 23*3600+56*60+4
+w_sid = 2*np.pi/(Tsid)
+print(f'w_sid = {w_sid:.2e} rad.s^-1')
